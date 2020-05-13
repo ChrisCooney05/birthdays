be788c7f701116180a86f9edd046c13ff64c7636
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-13 11:34:59 +0100

    Stores birthdays in add method

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,3 +1,11 @@
 class BirthdayList
 
+  def initialize
+    @list = []
+  end
+
+  def add(name, dob)
+    @list.push(name: name, dob: dob)
+  end
+  
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -5,4 +5,10 @@ describe BirthdayList do
   it "Has a BirthdayList class" do
     expect(BirthdayList).to be
   end
+
+  it "Makes a new Birthday list and stores birthday" do
+    birthday_list = BirthdayList.new
+    expect(birthday_list.add("Chris", "19/02/1991")).to eq([name: "Chris", dob: "19/02/1991"])
+  end
+  
 end
